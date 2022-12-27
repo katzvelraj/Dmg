8b6a2d6d00de31afaffe0a99bdb8536730c2a181
katzvelraj <velraj460@gmail.com>
2022-12-27 19:04:27 +0530

    format code

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, LogBox} from 'react-native';
 import {Provider as StoreProvider} from 'react-redux';
 import store from './src/redux/Store';
 import HomeScreen from './src/screens/HomeScreen';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from 'redux-persist';
 let persistor = persistStore(store);
-
+LogBox.ignoreAllLogs();
 const App = () => {
   return (
     <StoreProvider store={store}>
